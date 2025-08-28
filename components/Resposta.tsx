5b1411e712ffea6f16dcdc29277554a8e152a8d6
mariolemos <lemosmm64@gmail.com>
2025-08-28 14:40:08 -0300

    Criado componente Animação

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -11,11 +11,12 @@ interface RespostaProps {
 
 export default function Resposta(props: RespostaProps) {
 	const resposta = props.valor
+	const respostaRevelada = resposta.revelada ? styles.respostaRevelada : ''
 	return (
 		<div className={styles.resposta}
 			onClick={() => props.respostaFornecida(props.indice)}>
-			<div className={styles.conteudoResposta}>
-				{!resposta.revelada ? (
+			<div className={`${respostaRevelada} ${styles.conteudoResposta}`}>
+				
 					<div className={styles.frente}>
 						<div className={styles.letra}
 							style={{ backgroundColor: props.corFundoLetra }}>
@@ -24,8 +25,7 @@ export default function Resposta(props: RespostaProps) {
 						<div className={styles.valor}>
 							{resposta.valor}
 						</div>
-					</div>
-						) : (
+					</div>						
 							<div className={styles.verso}>
 								{resposta.certa ? (
 								<div className={styles.certa}>
@@ -39,7 +39,7 @@ export default function Resposta(props: RespostaProps) {
 								</div>
 								)}
 							</div>
-						)}
+						
 			</div>
 		</div>
 	)
